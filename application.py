52a8a33c5019ebe7fe6667d8034fd7078d5dfe8f
Becky Reamy <rlreamy@umich.edu>
2020-10-29 16:32:53 -0400

    KPMP-2166: Execute update statements

--- application.py
+++ application.py
@@ -1,12 +1,13 @@
 import flask
 from flask import request, jsonify
-from index_creator.core.index_creation import *
+from index_creator.core.index_creation import generate_index
+from index_creator.core.update_es import update_file_cases
 
 app = flask.Flask(__name__)
 
 @app.route('/api/v1/index/file_cases', methods=['GET'])
 def updateFileCases():
-
-    return jsonify(generate_index())
+    update_file_cases(generate_index())
+    return "ok"
 
 app.run()
