1da94052e8db12e4a3467b80fb1d7193d963fbeb
Becky Reamy <rlreamy@umich.edu>
2020-11-12 11:44:40 -0500

    KPMP-2166: Added lots of logging and fixed the update for all files

--- application.py
+++ application.py
@@ -1,23 +1,46 @@
 import flask
+import sys
 from index_creator.core.index_creation import generate_index
 from index_creator.core.update_es import update_file_cases
+import logging
 
-app = flask.Flask(__name__)
+app = flask.Flask('index-creation-worker')
+
+handler = logging.StreamHandler(sys.stdout)
+handler.setFormatter(logging.Formatter(
+    '%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+app.logger.addHandler(handler)
+app.logger.setLevel(logging.DEBUG)
 
 @app.route('/api/v1/index/file_cases', methods=['PUT'])
 def updateFileCases():
-    update_file_cases(generate_index())
-    return "ok"
+    try:
+        update_statement = generate_index()
+        update_file_cases(update_statement)
+        return "ok - updated with all files"
+    except:
+        app.logger.error(sys.exc_info()[0] + " on line: " + sys.exc_info()[-1].tb_lineno)
+        return "There was an error updating the index. Check the logs"
 
 @app.route('/api/v1/index/file_cases/file_id/<string:file_id>', methods=['PUT'])
 def updateFileCase(file_id):
-    update_file_cases(generate_index(file_id=file_id))
-    return "ok"
+    try:
+        update_statement = generate_index(file_id=file_id)
+        update_file_cases(update_statement)
+        return "ok - updated with file: " + file_id
+    except:
+        app.logger.error(sys.exc_info()[0])
+        return "There was an error updating the index. Check the logs"
 
 @app.route('/api/v1/index/file_cases/release_ver/<string:release_ver>', methods=['PUT'])
 def updateFileCaseRelease(release_ver):
-    update_file_cases(generate_index(release_ver=release_ver))
-    return "ok"
+    try:
+        update_statement = generate_index(release_ver=release_ver)
+        update_file_cases(update_statement)
+        return "ok - updated release: " + release_ver
+    except:
+        app.logger.error(sys.exc_info()[0])
+        return "There was an error updating the index. Check the logs"
 
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0')
+    app.run(host='0.0.0.0')
--- index_creator/core/update_es.py
+++ index_creator/core/update_es.py
@@ -1,7 +1,14 @@
-from elasticsearch import Elasticsearch
+from elasticsearch import Elasticsearch, ConnectionError, ElasticsearchException
+import logging
+
+log = logging.getLogger('index-creation-worker.update_file_cases')
 
 def update_file_cases(update_statement):
-    elastic = Elasticsearch(["elasticsearch"])
-    print(update_statement)
-    elastic.bulk(update_statement)
+    try:
+        elastic = Elasticsearch(['elasticsearch'])
+        elastic.bulk(update_statement)
+    except ConnectionError as connectionError:
+        log.error("error communicating with elasicsearch: " + str(connectionError))
+    except ElasticsearchException as e:
+        log.error("other error with elasticsearch: " + str(e))
 
