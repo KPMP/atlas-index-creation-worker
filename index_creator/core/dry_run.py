6b1a05c7a214159b697a6898cc559c5f01e96272
Dert1129 <nathancreger1111@gmail.com>
2023-01-06 10:50:41 -0500

    removed unuseful code

--- index_creator/core/dry_run.py
+++ index_creator/core/dry_run.py
@@ -1,7 +1,7 @@
 import mysql.connector
 import os
 import csv
-import sys
+import pandas as pd
 def generate_index(file_id = None, release_ver = None):
     mysql_user = os.environ.get('MYSQL_USER')
     mysql_pwd = os.environ.get('MYSQL_ROOT_PASSWORD')
@@ -28,7 +28,7 @@ def generate_updates(mydb, file_id = None, release_ver = None):
         elif release_ver is not None:
             where_clause = where_clause + " AND arf.release_version = " + str(release_ver) + " "
 
-        query = ("SELECT f.dl_file_id, p.redcap_id, p.sample_type, p.tissue_type, "
+        query = pd.read_sql_query("SELECT f.dl_file_id, p.redcap_id, p.sample_type, p.tissue_type, "
                 "p.age_binned, p.sex, d.doi, m.access, m.platform, m.experimental_strategy, "
                 "m.data_category, m.workflow_type, m.data_format, m.data_type, "
                 "f.file_name, f.file_size, p.protocol, f.package_id, p.tissue_source "
@@ -38,32 +38,14 @@ def generate_updates(mydb, file_id = None, release_ver = None):
                 "LEFT JOIN doi d ON fd.doi_id = d.doi_id "
                 "JOIN ar_file_info arf ON f.file_id = arf.file_id "
                 "JOIN metadata_type m ON arf.metadata_type_id = m.metadata_type_id " + where_clause +
-                "ORDER BY f.file_id");
+                "ORDER BY f.file_id", mydb);
 
-        mycursor.execute(query)
         csv_file_path = './ke_dump.csv'
-        rows = mycursor.fetchall()
+        df = pd.DataFrame(query)
+        df.to_csv(csv_file_path, index=False)
+        
+        
   finally:
     mydb.close()
-
-  if rows:
-      result = list()
-      column_names = list()
-      
-      for i in mycursor.description:
-          column_names.append(i[0])
-          
-      result.append(column_names)
-      for row in rows:
-          result.append(row)
-          
-      with open(csv_file_path, 'w', newline='') as csvfile:
-          csvwriter = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-          for row in result:
-              csvwriter.writerow(row)
-  else:
-      sys.exit("No rows found for query")
-  
-
 if __name__ == "__main__":
     generate_index()
