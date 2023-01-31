607abc5f5920c1f4a67c8255a8ce7ab6c5cbd435
dert1129 <nathancreger1111@gmail.com>
2023-01-31 15:06:37 -0500

    remove pandas

--- index_creator/core/dry_run.py
+++ index_creator/core/dry_run.py
@@ -1,12 +1,13 @@
 import mysql.connector
 import os
 import csv
-import pandas as pd
+from dotenv import load_dotenv
 def generate_index(file_id = None, release_ver = None):
-    mysql_user = os.environ.get('MYSQL_USER')
-    mysql_pwd = os.environ.get('MYSQL_ROOT_PASSWORD')
-    mysql_host = os.environ.get('MYSQL_HOST')
-
+    load_dotenv()
+    mysql_user = os.getenv('MYSQL_USER')
+    mysql_pwd = os.getenv('MYSQL_ROOT_PASSWORD')
+    mysql_host = os.getenv('MYSQL_HOST')
+    
     mydb = mysql.connector.connect(
         host=mysql_host,
         user=mysql_user,
@@ -28,7 +29,7 @@ def generate_updates(mydb, file_id = None, release_ver = None):
         elif release_ver is not None:
             where_clause = where_clause + " AND arf.release_version = " + str(release_ver) + " "
 
-        query = pd.read_sql_query("SELECT f.dl_file_id, p.redcap_id, p.sample_type, p.tissue_type, "
+        query = ("SELECT f.dl_file_id, p.redcap_id, p.sample_type, p.tissue_type, "
                 "p.age_binned, p.sex, d.doi, m.access, m.platform, m.experimental_strategy, "
                 "m.data_category, m.workflow_type, m.data_format, m.data_type, "
                 "f.file_name, f.file_size, p.protocol, f.package_id, p.tissue_source "
@@ -37,13 +38,16 @@ def generate_updates(mydb, file_id = None, release_ver = None):
                 "LEFT JOIN doi_files fd ON f.file_id = fd.file_id "
                 "LEFT JOIN doi d ON fd.doi_id = d.doi_id "
                 "JOIN ar_file_info arf ON f.file_id = arf.file_id "
-                "JOIN ar_file_info arf ON f.release_version = arf.release_version "
                 "JOIN metadata_type m ON arf.metadata_type_id = m.metadata_type_id " + where_clause +
-                "ORDER BY f.file_id", mydb);
-
+                "ORDER BY f.file_id");
+        mycursor.execute(query);
         csv_file_path = './ke_dump.csv'
-        df = pd.DataFrame(query)
-        df.to_csv(csv_file_path, index=False)
+        rows = mycursor.fetchall();
+        fp = open(csv_file_path, 'w')
+        myFile = csv.writere(fp)
+        myFile.writeheader()
+        myFile.writerows(rows)
+        fp.close()
         
         
   finally:
