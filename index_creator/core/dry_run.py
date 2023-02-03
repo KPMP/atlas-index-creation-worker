11f3d22f2f20831fce9089dfd69a7b51e53fdc85
Dert1129 <nathancreger1111@gmail.com>
2023-02-03 13:12:08 -0500

    add loop

--- index_creator/core/dry_run.py
+++ index_creator/core/dry_run.py
@@ -1,13 +1,12 @@
 import mysql.connector
 import os
 import csv
-from dotenv import load_dotenv
+import pandas as pd
 def generate_index(file_id = None, release_ver = None):
-    load_dotenv()
-    mysql_user = os.getenv('MYSQL_USER')
-    mysql_pwd = os.getenv('MYSQL_ROOT_PASSWORD')
-    mysql_host = os.getenv('MYSQL_HOST')
-    
+    mysql_user = os.environ.get('MYSQL_USER')
+    mysql_pwd = os.environ.get('MYSQL_ROOT_PASSWORD')
+    mysql_host = os.environ.get('MYSQL_HOST')
+
     mydb = mysql.connector.connect(
         host=mysql_host,
         user=mysql_user,
@@ -21,15 +20,13 @@ def generate_index(file_id = None, release_ver = None):
 
 def generate_updates(mydb, file_id = None, release_ver = None):
   try:
-        mycursor = mydb.cursor(buffered=True, dictionary=True)
-
         where_clause = " WHERE arf.release_sunset_version is NULL ";
         if file_id is not None:
             where_clause = where_clause + " AND f.dl_file_id = '" + str(file_id) + "' "
         elif release_ver is not None:
             where_clause = where_clause + " AND arf.release_version = " + str(release_ver) + " "
 
-        query = ("SELECT f.dl_file_id, p.redcap_id, p.sample_type, p.tissue_type, "
+        query = pd.read_sql_query("SELECT f.dl_file_id, p.redcap_id, p.sample_type, p.tissue_type, "
                 "p.age_binned, p.sex, d.doi, m.access, m.platform, m.experimental_strategy, "
                 "m.data_category, m.workflow_type, m.data_format, m.data_type, "
                 "f.file_name, f.file_size, p.protocol, f.package_id, p.tissue_source "
@@ -39,15 +36,18 @@ def generate_updates(mydb, file_id = None, release_ver = None):
                 "LEFT JOIN doi d ON fd.doi_id = d.doi_id "
                 "JOIN ar_file_info arf ON f.file_id = arf.file_id "
                 "JOIN metadata_type m ON arf.metadata_type_id = m.metadata_type_id " + where_clause +
-                "ORDER BY f.file_id");
-        mycursor.execute(query);
+                "ORDER BY f.file_id", mydb);
+
         csv_file_path = './ke_dump.csv'
-        rows = mycursor.fetchall();
-        fp = open(csv_file_path, 'w')
-        myFile = csv.writere(fp)
-        myFile.writeheader()
-        myFile.writerows(rows)
-        fp.close()
+        df = pd.DataFrame(query)
+        df.to_csv(csv_file_path, index=False)
+        df = pd.read_csv(csv_file_path)
+        version = 0
+        while version < 4:
+            version += 1
+            query = pd.read_sql_query("SELECT COUNT(*) FROM file f JOIN ar_file_info afi ON f.file_id = afi.file_id WHERE release_version= " + version, mydb)
+            df["version " + version + " count"] = query
+            df.to_csv(csv_file_path, index=False)
         
         
   finally:
