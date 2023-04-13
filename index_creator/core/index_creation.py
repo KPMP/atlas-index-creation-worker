93ca0140e831e5547c9223eb587f0152412b2bf0
rlreamy <rlreamy@umich.edu>
2023-04-13 14:30:19 -0400

    KPMP-4008: get flattened structure

--- index_creator/core/index_creation.py
+++ index_creator/core/index_creation.py
@@ -2,6 +2,7 @@ import json
 import mysql.connector
 import os
 from index_creator.models.FileCasesIndexDoc import FileCasesIndexDoc
+from index_creator.models.EnterpriseSearchIndexDoc import EnterpriseSearchIndexDoc
 from index_creator.models.IndexDoc import IndexDoc
 import logging
 
@@ -22,6 +23,79 @@ def get_index_doc_json(index_doc):
     except TypeError as err:
         log.error(err)
     return doc
+
+def get_enterprise_index_json(index_doc):
+    try:
+        index_doc.dois = list(index_doc.dois)
+        json_doc = json.dumps(index_doc.__dict__)
+        return json_doc
+    except TypeError as err:
+        log.error(err)
+
+
+def generate_json(mydb, file_id = None, release_ver = None):
+    try:
+        values = ()
+        mycursor = mydb.cursor(buffered=True, dictionary=True)
+
+        where_clause = " WHERE arf.release_sunset_version is NULL ";
+        if file_id is not None:
+            where_clause = where_clause + " AND f.dl_file_id = '" + str(file_id) + "' "
+        elif release_ver is not None:
+            where_clause = where_clause + " AND arf.release_version = CAST(%s AS FLOAT) "
+            values = (str(release_ver),)
+
+        query = ("SELECT f.dl_file_id, p.redcap_id, p.sample_type, p.tissue_type, "
+                "p.age_binned, p.sex, d.doi, m.access, m.platform, m.experimental_strategy, "
+                "m.data_category, m.workflow_type, m.data_format, m.data_type, "
+                "f.file_name, f.file_size, p.protocol, f.package_id, p.tissue_source "
+                "FROM file f JOIN file_participant fp ON f.file_id = fp.file_id "
+                "JOIN participant p ON fp.participant_id = p.participant_id "
+                "LEFT JOIN doi_files fd ON f.file_id = fd.file_id "
+                "LEFT JOIN doi d ON fd.doi_id = d.doi_id "
+                "JOIN ar_file_info arf ON f.file_id = arf.file_id "
+                "JOIN metadata_type m ON arf.metadata_type_id = m.metadata_type_id " + where_clause +
+                " ORDER BY f.file_id");
+
+        mycursor.execute(query, values)
+        documents = {}
+        if not mycursor.rowcount:
+            log.warning("query returned 0 results. No updates to process")
+            return "";
+
+        repository_json = {}
+        for row in mycursor:
+
+            # If we already have a document for this file, add information to it
+            if row["dl_file_id"] in documents:
+                index_doc = documents[row["dl_file_id"]]
+                # Not adding a new tissue source because we should only have one tissue source per file
+                index_doc.participant_id.append(row['redcap_id'])
+                index_doc.sample_type.append(row['sample_type'])
+                index_doc.tissue_type.append(row['tissue_type'])
+                index_doc.protocol.append(row['protocol'])
+                index_doc.age_binned.append(row['age_binned'])
+                index_doc.sex.append(row['sex'])
+                index_doc.tissue_source.append(row['tissue_source'])
+                index_doc.dois.add(row['doi'])
+            # If this is a new file, then we need to create the initial record and add it to our list of documents
+            else:
+                index_doc = EnterpriseSearchIndexDoc(row["access"], row["platform"], row["experimental_strategy"], row["data_category"],
+                                     row["workflow_type"], row["data_format"], row["data_type"], row["dl_file_id"],
+                                     row["file_name"], row["file_size"], row["package_id"], {row["doi"]}, row['redcap_id'], [row['sample_type']],
+                                     [row['tissue_type']], [row['protocol']], [row['sex']], [row['age_binned']], [row['tissue_source']], [row['redcap_id']])
+                documents[row["dl_file_id"]] = index_doc
+
+        update_statement = '';
+        for id in documents:
+            update_statement = update_statement + get_enterprise_index_json(documents[id])
+        try:
+            return update_statement
+        except NameError:
+            log.error("Unable to process results");
+            pass
+    finally:
+        mycursor.close()
 
 def generate_updates(mydb, file_id = None, release_ver = None):
     try:
@@ -164,6 +238,26 @@ def generate_index(file_id = None, release_ver = None):
         mydb.close();
 
     return es_update_statement;
+
+def generate_repo_index():
+    mysql_user = os.environ.get('MYSQL_USER')
+    mysql_pwd = os.environ.get('MYSQL_ROOT_PASSWORD')
+    mysql_host = os.environ.get('MYSQL_HOST')
+
+    mydb = mysql.connector.connect(
+            host=mysql_host,
+            user=mysql_user,
+            password=mysql_pwd,
+            database="knowledge_environment",
+            converter_class=MyConverter
+    )
+    try:
+        update_statement = generate_json(mydb)
+    finally:
+        mydb.close()
+
+    return update_statement
+
 
 class MyConverter(mysql.connector.conversion.MySQLConverter):
 
---
+++ index_creator/models/EnterpriseSearchIndexDoc.py
@@ -0,0 +1,24 @@
+class EnterpriseSearchIndexDoc:
+    def __init__(self, access, platform, experimental_strategy, data_category, workflow_type, data_format,
+                 data_type, file_id, file_name, file_size, package_id, dois, redcap_id, sample_type, tissue_type,
+                 protocol, sex, age_binned, tissue_source, participant_id):
+        self.access = access
+        self.platform = platform
+        self.experimental_strategy = experimental_strategy
+        self.data_category = data_category
+        self.workflow_type = workflow_type
+        self.file_id = file_id
+        self.file_name = file_name
+        self.data_format = data_format
+        self.file_size = file_size
+        self.data_type = data_type
+        self.package_id = package_id
+        self.dois = dois
+        self.redcap_id = redcap_id
+        self.sample_type = sample_type
+        self.tissue_type = tissue_type
+        self.protocol = protocol
+        self.sex = sex
+        self.age_binned = age_binned
+        self.tissue_source = tissue_source
+        self.participant_id = participant_id
